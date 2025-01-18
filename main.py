b32ee0a6a48f62b909069caef96e0408d61194f5
AbdKaan <abdu.kaan@gmail.com>
2025-01-18 18:56:59 +0300

    Maze class

--- main.py
+++ main.py
@@ -1,29 +1,19 @@
 from graphics import *
-from cell import Cell
+from maze import Maze
 
 
 def main():
     win = Window(800, 600)
 
-    cell = Cell(win)
-    cell.has_left_wall = False
-    cell.draw(100, 200, 100, 200)
-
-    cell_two = Cell(win)
-    cell_two.has_top_wall = False
-    cell_two.draw(200, 300, 100, 200)
-
-    cell_three = Cell(win)
-    cell_three.has_bottom_wall = False
-    cell_three.draw(300, 400, 100, 200)
-
-    cell_four = Cell(win)
-    cell_four.draw(600, 400, 300, 600)
-
-    cell.draw_move(cell_two)
-    cell_three.draw_move(cell_four)
-
-    cell.draw_move(cell_two, undo=True)
+    maze = Maze(
+        x1=100,
+        y1=100,
+        num_rows=10,
+        num_cols=10,
+        cell_size_x=60,
+        cell_size_y=40,
+        win=win
+    )
 
     win.wait_for_close()
 
